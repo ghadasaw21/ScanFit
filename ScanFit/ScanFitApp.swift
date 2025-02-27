ce8ba7ca5e473e6fd67da9d6c9a1ff30affc45b2
ghadasaw21 <academyghadaphone@icloud.com>
2025-02-27 14:58:52 +0300

    Initial Commit

---
+++ ScanFit/ScanFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanFitApp.swift
+//  ScanFit
+//
+//  Created by Ghada al ajmi on 28/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScanFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
